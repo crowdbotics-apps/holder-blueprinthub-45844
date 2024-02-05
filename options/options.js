3cfbd904c40b5fc0113047cb5e428694e38c44b0
Crowdbotics <team@crowdbotics.com>
2024-02-05 03:54:20 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "holder-blueprinthub-45844",
+  url: "https://holder-blueprinthub-45844.botics.co",
+  api: "https://holder-blueprinthub-45844.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
